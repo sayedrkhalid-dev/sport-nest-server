ed94edb76d76038129c56bf25670cc2a556eb79f
Sayed <sayedrkhalid.official@gmail.com>
2026-06-07 22:59:59 +0600

    fixed front-end url

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const bookingRoutes = require("./booking/booking.routes");
 const app = express();
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL",
+  origin: process.env.FRONTEND_URL || "http://localhost:3000",
   credentials: true,
 }));
 app.use(express.json());
